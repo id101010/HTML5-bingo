72be111c9a8ea8caad4d5bc9302033f0f0061bd5
Aaron <aaron@duckpond.ch>
2021-05-21 16:10:31 +0200

    pmt touch

--- js/data.js
+++ js/data.js
@@ -1,43 +1,70 @@
-var headerText = "Stuff Republicans Say";
+var headerText = "Polizeiliche Massnahmen zur Bekämpfung von Terrorismus Bullshit Bingo";
 
-var footerText = "<a href='https://github.com/michaellevesque/HTML5-bingo' target='_blank'>Code available on github</a>";
+var footerText = "<a href='https://github.com/id101010/HTML5-bingo/' target='_blank'>This versions is code available on github</a><br/><a href='https://github.com/jeffehobbs/HTML5-bingo/' target='_blank'>Original code</a>";
 
-var winText = "Winner";
+var winText = "PMT Bullshit erfolgreich maximiert!";
 
 var clickSnd = new Audio("audio/click.mp3");
 
 var winSnd = new Audio("audio/win.mp3");
 
 var JSONBingo = {"squares": [
-        {"square": "Only Atheists can get AIDS."},
-        {"square": "There would be less rape if all women were homemakers."},
-        {"square": "You can't get pregnant from Christmas rape."},
-        {"square": "Widespread contraception use leads to increased incidence of rape."},
-        {"square": "If you read 50 Shades of Grey, your going to hell."},
-        {"square": "Dads who conceive children through rape should have full visitation rights."},
-        {"square": "Homosexuality is a choice."},
-        {"square": "Everyone would be tempted to be gay if we didn't shame homosexuality."},
-        {"square": "There was not rape before feminism."},
-        {"square": "Marriage cures AIDS."},
-        {"square": "Church Pedophilia scandals result from tolerance of homosexuals."},
-        {"square": "Girls who watch Girls will never be happy wives."},
-        {"square": "Women subconsciously want to be raped; they send chemical signals that trigger men."},
-        {"square": "If we teach kids to put condoms on bananas, we might as well teach them how to use stripper poles."},
-        {"square": "If a girl gets drunk at a party, well, you know, I'm just sayin'."},
-        {"square": "This just shows that FemiNazi agenda is false."},
-        {"square": "Want to stop date rape? Stop Dating."},
-        {"square": "You can't take back the night; you never had it in the first place."},
-        {"square": "Middle school health class turn girls into prostitutes."},
-        {"square": "The solution to rape in the military is to kick out the women."},
-        {"square": "You want to know that your fellow soldier's got you back, not that he's staring at it."},
-        {"square": "Feminism's secret goal is to see all Christian boys aborted."},
-        {"square": "Well she has had many prior sexual partners you know."},
-        {"square": "If we're going to teach kids safe sex, we might as well just inject them with AIDS."},            
-        {"square": "If we stopped all imports the US economy would be stronger"},    	
-        {"square": "Immigration is the problem, cheap labor corrupts."},    	
-        {"square": "China has spies in all US companies, stealing our ideas."}, 
-        {"square": "If your CEO is not republican he is a net drain on the economy."},    	
-        {"square": "Farm subsides are beneficial because they help the economy."},    	
-        {"square": "If Marijuana was legal all our kids would use it."} 	
+        {"square": "Mehr Transparenz!"},
+        {"square": "Plenum soll entscheiden"},
+        {"square": "Vorstand soll entscheiden"},
+        {"square": "Da gibt es einen Beschluss"},
+        {"square": "Ad hominem"},
+        {"square": "Ad personam"},
+        {"square": "Moving the goalposts"},
+        {"square": "Anekdotische Evidenz"},
+        {"square": "Berufung auf Wahrscheinlich-keit"},
+        {"square": "Strohmann"},
+        {"square": "Argumentum ad ignorantiam"},
+        {"square": "Ad nauseam"},
+        {"square": "Petitio principii"},
+        {"square": "Beweislast-umkehr"},
+        {"square": "Zirkelschluss"},
+        {"square": "Korrelation und Kausation verwechselt"},
+        {"square": "Argumentum ad verecundiam"},
+        {"square": "Falsches Dilemma"},
+        {"square": "Fangfrage"},
+        {"square": "Spieler-fehlschluss"},
+        {"square": "Unvollst&auml;ndiger Vergleich"},
+        {"square": "(Fehl)-interpretation der Intention"},
+        {"square": "Ignoratio elenchi"},
+        {"square": "Moral high ground"},
+        {"square": "Natura-listischer Fehlschluss"},
+        {"square": "Post hoc, ergo propter hoc"}, 
+        {"square": "Cum hoc, ergo propter hoc"},
+        {"square": "Beweis durch Wiederholung"},
+        {"square": "Argumentum verbosium"},
+        {"square": "Berufung auf Emotionen"},
+        {"square": "Argumentum ad novitatem"},
+        {"square": "Argumentum ad antiquitatem"},
+        {"square": "Argumentum ad baculum"},
+        {"square": "Argumentum ad populum"},
+        {"square": "First world problem"},
+        {"square": "Mimimi auf hohem Niveau"},
+        {"square": "Pis erstes Gesetz"},
+        {"square": "Pis zweites Gesetz"},
+        {"square": "Godwin"},
+        {"square": "Doppelt falsch ist gut"},
+        {"square": "Broken window"},
+        {"square": "Slippery slope"},
+        {"square": "Blockwart schreitet ein"},
+        {"square": "Argumentum ad mailclient"},
+        {"square": "Wer hat Schuld"},
+        {"square": "Wer ist denn Verantwortlich"},
+        {"square": "Rehctschreibung"},  // absichtsvoller Schreibfehler
+        {"square": "So hab ich das nicht gesagt"},
+        {"square": "Fr&uuml;her haben wir das auch so gemacht"},
+        {"square": "Confirmation Bias"},
+        {"square": "tu quoque"},
+        {"square": "Mereologischer Fehlschluss"},
+        {"square": "Argument ad temperantiam"}, // übersetzt als: siehe nächste Zeile
+        {"square": "Schiedrichter-Fehlschluss"},
+        {"square": "Guilt by Association"},
+        {"square": "Argument ex silentio"},  // http://www.hoheluft-magazin.de/2016/08/na-logisch-das-argument-ex-silentio/
+        {"square": "Psychologistischer Fehlschluss"} // http://www.hoheluft-magazin.de/2016/10/na-logisch-der-psychologistische-fehlschluss/
     ]
 };
